b42980d53551b5f2b7c4a34cc4c79411d19f15bc
git.Ball <georgiballabanov@gmail.com>
2022-02-13 13:18:44 +0200

    Added some updates.

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-// import { getData } from "./api.mjs";
 import { loadPage } from "./load.js";
-
-
 import { render, page, html } from "./util.js";
 
-alert("bau");
+
 
 const root = document.querySelector("#loadMore");
 
@@ -42,56 +39,21 @@ const topDiv = document
 const widthOutput = document.querySelector("#width");
 
 export function reportWindowSize() {
-  widthOutput.textContent = window.innerWidth; //?
-  console.log(widthOutput.textContent);
+  widthOutput.textContent = window.innerWidth;
   if (widthOutput.textContent < 760) {
     return gliderDowngrade();
   } else {
     return gliderUpdate();
   }
 }
-// window.onresize = reportWindowSize;
-// window.addEventListener('resize', reportWindowSize);
-// reportWindowSize()
+
 
 document.body.onresize = reportWindowSize;
-// document.addEventListener('resize', reportWindowSize);
 
-// let clientWidth = document.body.clientWidth
-// clientWidth.addEventListener('resize', reportWindowSize);
-
-// intElemClientWidth.addEventListener('resize', reportWindowSize);
 reportWindowSize();
 
-// let loadMoreBtn = document
-//   .querySelector(".loadMore")
-//   .addEventListener("click", loadPage)
 
 
-    // let data = await getData();
-    // const template = (img) => html`<img class="event-img" src="${img}" />`;
-
-    // const lastImg = data[0].img;
-    // console.log(lastImg)
-    // reportWindowSize();
-    // let hiddenEl = document.querySelector("#showMore");
-    // hiddenEl.classList.remove("hidden");
-    // const btn = document.querySelector(".loadMore").classList.add("hidden");
-    // render(template(lastImg));
-  // });
-
-  // async function load(){
-  //   let data = await getData();
-  //   const template = (img) => html`<img class="event-img" src="${img}" />`;
-
-  //   const lastImg = data[0].img;
-  //   console.log(lastImg)
-  //   reportWindowSize();
-  //   let hiddenEl = document.querySelector("#showMore");
-  //   hiddenEl.classList.remove("hidden");
-  //   const btn = document.querySelector(".loadMore").classList.add("hidden");
-  //   return ctx.render(template(lastImg));
-  // }
 function gliderUpdate() {
   return new Glider(document.querySelector(".glider"), {
     slidesToShow: 3,
